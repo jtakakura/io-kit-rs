ee9f0f0aed4f61a29934742bd23e4c8b8f48effc
Junji Takakura <j.takakura@gmail.com>
2017-09-09 14:42:53 +0900

    Add IOHIDValue

--- src/hid/mod.rs
+++ src/hid/mod.rs
@@ -4,3 +4,4 @@ pub mod element;
 pub mod keys;
 pub mod manager;
 pub mod usage_tables;
+pub mod value;
---
+++ src/hid/value.rs
@@ -0,0 +1,42 @@
+// exports from <IOKit/hid/IOHIDValue.h>
+
+use cf::base::{CFAllocatorRef, CFIndex, CFTypeID};
+
+use hid::base::{IOHIDElementRef, IOHIDValueRef};
+use hid::keys::IOHIDValueScaleType;
+
+extern "C" {
+    pub fn IOHIDValueGetTypeID() -> CFTypeID;
+
+    pub fn IOHIDValueCreateWithIntegerValue(allocator: CFAllocatorRef,
+                                            element: IOHIDElementRef,
+                                            timeStamp: u64,
+                                            value: CFIndex)
+                                            -> IOHIDValueRef;
+
+    pub fn IOHIDValueCreateWithBytes(allocator: CFAllocatorRef,
+                                     element: IOHIDElementRef,
+                                     timeStamp: u64,
+                                     bytes: *const u8,
+                                     length: CFIndex)
+                                     -> IOHIDValueRef;
+
+    pub fn IOHIDValueCreateWithBytesNoCopy(allocator: CFAllocatorRef,
+                                           element: IOHIDElementRef,
+                                           timeStamp: u64,
+                                           bytes: *const u8,
+                                           length: CFIndex)
+                                           -> IOHIDValueRef;
+
+    pub fn IOHIDValueGetElement(value: IOHIDValueRef) -> IOHIDElementRef;
+
+    pub fn IOHIDValueGetTimeStamp(value: IOHIDValueRef) -> u64;
+
+    pub fn IOHIDValueGetLength(value: IOHIDValueRef) -> CFIndex;
+
+    pub fn IOHIDValueGetBytePtr(value: IOHIDValueRef) -> *const u8;
+
+    pub fn IOHIDValueGetIntegerValue(value: IOHIDValueRef) -> CFIndex;
+
+    pub fn IOHIDValueGetScaledValue(value: IOHIDValueRef, type_: IOHIDValueScaleType) -> f64;
+}
